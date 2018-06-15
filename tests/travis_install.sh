200251cc58fcebfbc88f7fad9db629f858330ec8
Anderson Bravalheri <andersonbravalheri@gmail.com>
2018-06-15 22:00:02 +0000

    Add cache of conda installation to Travis

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -15,25 +15,44 @@ if [[ "$DISTRIB" == "conda" ]]; then
     # conda-based environment instead
     deactivate
 
-    # Use the miniconda installer for faster download / install of conda
-    # itself
-    wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-        -O miniconda.sh
-    chmod +x miniconda.sh && ./miniconda.sh -b -p $HOME/miniconda
+    if [[ -f "$HOME/miniconda/bin/conda" ]]; then
+        echo "Skip install conda"
+    else
+      rm -rf "$HOME/miniconda"
+      # ^  Caching creates empty dir that conflicts with conda installer
+
+      # Use the miniconda installer for faster download / install of conda
+      # itself
+      wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+          -O miniconda.sh
+      chmod +x miniconda.sh && ./miniconda.sh -b -p $HOME/miniconda
+    fi
     export PATH=$HOME/miniconda/bin:$PATH
     conda update --yes conda
 
     # Configure the conda environment and put it in the path using the
     # provided versions
-    conda create -n testenv --yes python=${PYTHON_VERSION} pip virtualenv
-    source activate testenv
+    conda create -p ./.venv --yes python=${PYTHON_VERSION} pip virtualenv
+    source activate ./.venv
 fi
 
 
 if [[ "$COVERAGE" == "true" ]]; then
     pip install coverage coveralls
 fi
+
 # for all
 pip install -U pip setuptools
 pip install sphinx
 pip install tox
+
+travis-cleanup() {
+  printf "Cleaning up environments ... "  # printf avoids new lines
+  if [[ "$DISTRIB" == "conda" ]]; then
+    # Force the env to be recreated next time, for build consistency
+    source deactivate
+    conda remove -p ./.venv --all --yes
+    rm -rf ./.venv
+  fi
+  echo "DONE"
+}
