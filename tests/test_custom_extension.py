ed41bd88e65c872ad4ffd5a4ff64be961361faee
Florian Wilhelm <florian.wilhelm@inovex.de>
2019-06-19 17:32:01 +0200

    Recreate this extentions using the new custom extension

---
+++ tests/test_custom_extension.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from os.path import exists as path_exists
+
+from pyscaffold.api import create_project
+from pyscaffold.cli import parse_args
+from pyscaffold.utils import chdir
+
+from pyscaffoldext.pyproject.extension import Pyproject
+
+EXT_FLAG = "--pyproject"
+
+
+def test_add_custom_extension(tmpfolder):
+    opts = {"package": "my_extension", "project": "my_project",
+            "extensions": [Pyproject("custom_ext")]}
+    create_project(opts)
+    assert path_exists("my_project/src/my_extension/extension.py")
+
+
+def test_add_custom_extension_with_namespace(tmpfolder):
+    args = ["--namespace", "ns", EXT_FLAG, "my_project"]
+
+    opts = parse_args(args)
+    create_project(opts)
+    assert path_exists("my_project/src/ns/my_extension/extension.py")
+
+
+def test_add_custom_extension_with_namespace_2(tmpfolder):
+    args = ["--namespace", "ns1.ns2", EXT_FLAG, "my_project"]
+
+    opts = parse_args(args)
+    create_project(opts)
+    assert path_exists("my_project/src/ns1/ns2/my_project/extension.py")
+
+
+def test_generated_extension_flake8(tmpfolder, venv_run):
+    args = [EXT_FLAG, "my_project"]
+
+    opts = parse_args(args)
+    create_project(opts)
+
+    with chdir("my_project"):
+        assert '' == venv_run("flake8")
+        venv_run("python setup.py install")
+
+    venv_run("putup {ext_flag} the_actual_project".format(EXT_FLAG))
+    assert path_exists("the_actual_project/setup.cfg")
+
+    with chdir("the_actual_project"):
+        assert '' == venv_run("flake8")
--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -4,7 +4,7 @@
 # The behavior of the script is controlled by environment variabled defined
 # in the .travis.yml in the top level folder of the project.
 #
-# This script is taken from Scikit-Learn (http://scikit-learn.org/)
+# This script is inspired by Scikit-Learn (http://scikit-learn.org/)
 #
 # THIS SCRIPT IS SUPPOSED TO BE AN EXAMPLE. MODIFY IT ACCORDING TO YOUR NEEDS!
 
@@ -16,43 +16,42 @@ if [[ "$DISTRIB" == "conda" ]]; then
     deactivate
 
     if [[ -f "$HOME/miniconda/bin/conda" ]]; then
-        echo "Skip install conda"
+        echo "Skip install conda [cached]"
     else
-      rm -rf "$HOME/miniconda"
-      # ^  Caching creates empty dir that conflicts with conda installer
+        # By default, travis caching mechanism creates an empty dir in the
+        # beginning of the build, but conda installer aborts if it finds an
+        # existing folder, so let's just remove it:
+        rm -rf "$HOME/miniconda"
 
-      # Use the miniconda installer for faster download / install of conda
-      # itself
-      wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-          -O miniconda.sh
-      chmod +x miniconda.sh && ./miniconda.sh -b -p $HOME/miniconda
+        # Use the miniconda installer for faster download / install of conda
+        # itself
+        wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+            -O miniconda.sh
+        chmod +x miniconda.sh && ./miniconda.sh -b -p $HOME/miniconda
     fi
     export PATH=$HOME/miniconda/bin:$PATH
+    # Make sure to use the most updated version
     conda update --yes conda
 
     # Configure the conda environment and put it in the path using the
     # provided versions
+    # (prefer local venv, since the miniconda folder is cached)
     conda create -p ./.venv --yes python=${PYTHON_VERSION} pip virtualenv
     source activate ./.venv
 fi
 
-
 if [[ "$COVERAGE" == "true" ]]; then
-    pip install coverage coveralls pre-commit
+    pip install coverage coveralls
 fi
 
-# for all
-pip install -U pip setuptools
-pip install sphinx
-pip install tox
 
 travis-cleanup() {
-  printf "Cleaning up environments ... "  # printf avoids new lines
-  if [[ "$DISTRIB" == "conda" ]]; then
-    # Force the env to be recreated next time, for build consistency
-    source deactivate
-    conda remove -p ./.venv --all --yes
-    rm -rf ./.venv
-  fi
-  echo "DONE"
+    printf "Cleaning up environments ... "  # printf avoids new lines
+    if [[ "$DISTRIB" == "conda" ]]; then
+        # Force the env to be recreated next time, for build consistency
+        source deactivate
+        conda remove -p ./.venv --all --yes
+        rm -rf ./.venv
+    fi
+    echo "DONE"
 }
