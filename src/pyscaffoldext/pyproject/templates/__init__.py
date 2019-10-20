66e89f931a4a5f13453720dec018afbd3ed79bd9
Florian Wilhelm <Florian.Wilhelm@inovex.de>
2019-10-20 11:24:28 +0200

    Reuse PyScaffold template function

--- src/pyscaffoldext/pyproject/templates/__init__.py
+++ src/pyscaffoldext/pyproject/templates/__init__.py
@@ -2,25 +2,7 @@
 """
 Templates for all files this extension provides
 """
-import os
-import string
-from pkg_resources import resource_string
-
-
-def get_template(name):
-    """Retrieve the template by name
-
-    Args:
-        name: name of template
-
-    Returns:
-        :obj:`string.Template`: template
-    """
-    file_name = "{name}.template".format(name=name)
-    data = resource_string(__name__, file_name)
-    # we assure that line endings are converted to '\n' for all OS
-    data = data.decode(encoding="utf-8").replace(os.linesep, '\n')
-    return string.Template(data)
+from pyscaffold.templates import get_template
 
 
 def pyproject_toml(opts):
